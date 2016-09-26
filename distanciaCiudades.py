2f6ca18396c6c602548bd23e7b19fb603f2b5318
PatricioIribarneCatella <patricio@patricio-Lenovo-V360>
2016-09-26 20:08:05 -0300

    Modificaciones varias

--- distanciaCiudades.py
+++ distanciaCiudades.py
@@ -1,134 +1,109 @@
-#Argentina. Buenos Aires
-# -34.61 	-58.38
-#Bolivia. Sucre
-# -19.03 	-65.26
-#Brasil. Brasalia
-# -15.78 	-47.93
-#Chile. Santiago
-# -33.46 	-70.65
-#Colombia. Bogota
-# 4.61 	-74.08
-#Ecuador. Quito
-# -0.23 	-78.52
-#Guyana. Georgetown
-#6.80 	-58.16
-#Paraguay. Asuncian
-#-25.30 	-57.64
-#Peru. Lima
-# 	-12.04 	-77.03
-#Surinam. Paramaribo
-#5.87 	-55.17
-#Uruguay. Montevideo
-# 	-34.83 	-56.17
-#Venezuela. Caracas
-# 	10.49 	-66.88
-
 from geopy.distance import great_circle
 
 capitalesEur =[
-#Albania Flag Icon 	Albania 	Tirana
+# Albania 	Tirana
 [41.33, 19.82],
-#Andorra Flag Icon 	Andorra 	Andorra la Vella
+# Andorra 	Andorra la Vella
 [42.51, 	1.52],
-#Austria Flag Icon 	Austria 	Vienna
+# Austria 	Vienna
 [48.21,	16.37],
-#Belarus Flag Icon 	Belarus 	Minsk
+# Belarus 	Minsk
 [53.90,	27.57],
-#[Belgium Flag Icon 	Belgium 	Brussels
+# Belgium 	Brussels
 [50.85,	4.35],
-#[Bosnia and Herzegovina Flag Icon 	Bosnia and Herzegovina 	Sarajevo
+# Bosnia and Herzegovina 	Sarajevo
 [43.85, 	18.36],
-#[Bulgaria Flag Icon 	Bulgaria 	Sofia
+# Bulgaria 	Sofia
 [42.70, 	23.32],
-#[Croatia Flag Icon 	Croatia 	Zagreb
+# Croatia 	Zagreb
 [45.81, 	15.98],
-#[Cyprus Flag Icon 	Cyprus 	Nicosia
+# Cyprus 	Nicosia
 [35.17, 	33.37],
-#[Czech Republic Flag Icon 	Czech Republic 	Prague
+# Czech Republic 	Prague
 [50.09, 	14.42],
-#[Denmark Flag Icon 	Denmark 	Copenhagen
+# Denmark 	Copenhagen
 [55.68, 	12.57],
-#[Estonia Flag Icon 	Estonia 	Tallinn
+# Estonia 	Tallinn
 [59.44, 	24.75],
-#[Faroe Islands Flag Icon 	Faroe Islands 	Torshavn
+# Faroe Islands 	Torshavn
 [62.01, 	-6.77],
-#[Finland Flag Icon 	Finland 	Helsinki
+# Finland 	Helsinki
 [60.17, 	24.94],
-#[France Flag Icon 	France 	Paris
+# France 	Paris
 [48.85, 	2.35],
-#[Germany Flag Icon 	Germany 	Berlin
+# Germany 	Berlin
 [52.52, 	13.41],
-#[Gibraltar Flag Icon 	Gibraltar 	Gibraltar
+# Gibraltar 	Gibraltar
 [36.14, 	-5.35],
-#[Greece Flag Icon 	Greece 	Athens
+# Greece 	Athens
 [37.98, 	23.72],
-#[Guernsey Flag Icon 	Guernsey 	St Peter Port
+# Guernsey 	St Peter Port
 [49.46, 	-2.54],
-#[Hungary Flag Icon 	Hungary 	Budapest
+# Hungary 	Budapest
 [47.50, 	19.04],
-#[Iceland Flag Icon 	Iceland 	Reykjavik
+# Iceland 	Reykjavik
 [64.14, 	-21.90],
-#[Ireland Flag Icon 	Ireland 	Dublin
+# Ireland 	Dublin
 [53.33, 	-6.25],
-#[Isle of Man Flag Icon 	Isle of Man 	Douglas
+# Isle of Man 	Douglas
 [54.15, 	-4.48],
-#[Italy Flag Icon 	Italy 	Rome
+# Italy 	Rome
 [41.89, 	12.48],
-#[Jersey Flag Icon 	Jersey 	Saint Helier
+# Jersey 	Saint Helier
 [49.19, 	-2.10],
-#[Kosovo Flag Icon 	Kosovo 	Pristina
+# Kosovo 	Pristina
 [42.67, 	21.17],
-#[Latvia Flag Icon 	Latvia 	Riga
+# Latvia 	Riga
 [56.95, 	24.11],
-#[Liechtenstein Flag Icon 	Liechtenstein 	Vaduz
+# Liechtenstein 	Vaduz
 [47.14, 	9.52],
-#[Lithuania Flag Icon 	Lithuania 	Vilnius
+# Lithuania 	Vilnius
 [54.69, 	25.28],
-#[Luxembourg Flag Icon 	Luxembourg 	Luxembourg
+# Luxembourg 	Luxembourg
 [49.61, 	6.13],
-#[Macedonia Flag Icon 	Macedonia 	Skopje
+# Macedonia 	Skopje
 [42.00, 	21.43],
-#[Malta Flag Icon 	Malta 	Valletta
+# Malta 	Valletta
 [35.90, 	14.51],
-#[Moldova Flag Icon 	Moldova 	Chisinau
+# Moldova 	Chisinau
 [47.01, 	28.86],
-#[Monaco Flag Icon 	Monaco 	Monaco
+# Monaco 	Monaco
 [43.73, 	7.42],
-#[Montenegro Flag Icon 	Montenegro 	Podgorica
+# Montenegro 	Podgorica
 [42.44, 	19.26],
-#[Netherlands Flag Icon 	Netherlands 	Amsterdam
+# Netherlands 	Amsterdam
 [52.37, 	4.89],
-#[Norway Flag Icon 	Norway 	Oslo
+# Norway 	Oslo
 [59.91, 	10.75],
-#[Poland Flag Icon 	Poland 	Warsaw
+# Poland 	Warsaw
 [52.23, 	21.01],
-#[Portugal Flag Icon 	Portugal 	Lisbon
+# Portugal 	Lisbon
 [38.72, 	-9.13],
-#[Romania Flag Icon 	Romania 	Bucharest
+# Romania 	Bucharest
 [44.43, 	26.11],
-#[Russia Flag Icon 	Russia 	Moscow
+# Russia 	Moscow
 [55.75, 	37.62],
-#[San Marino Flag Icon 	San Marino 	San Marino
+# San Marino 	San Marino
 [43.94, 	12.45],
-#[Serbia Flag Icon 	Serbia 	Belgrade
+# Serbia 	Belgrade
 [44.80, 	20.47],
-#[Slovakia Flag Icon 	Slovakia 	Bratislava
+# Slovakia 	Bratislava
 [48.15, 	17.11],
-#[Slovenia Flag Icon 	Slovenia 	Ljubljana
+# Slovenia 	Ljubljana
 [46.05, 	14.51],
-#[Spain Flag Icon 	Spain 	Madrid
+# Spain 	Madrid
 [40.42, 	-3.70],
-#[Svalbard and Jan Mayen Flag Icon 	Svalbard and Jan Mayen 	Longyearbyen
+# Svalbard and Jan Mayen 	Longyearbyen
 [78.22, 	15.64],
-#[Sweden Flag Icon 	Sweden 	Stockholm
+# Sweden 	Stockholm
 [59.33, 	18.06],
-#[Switzerland Flag Icon 	Switzerland 	Berne
+# Switzerland 	Berne
 [46.95, 	7.45],
-#[Ukraine Flag Icon 	Ukraine 	Kiev
+# Ukraine 	Kiev
 [50.45, 	30.52],
-#[United Kingdom Flag Icon 	United Kingdom 	London
+# United Kingdom 	London
 [51.51, 	-0.13],
-#[Vatican Flag Icon 	Vatican 	Vatican
+# Vatican 	Vatican
 [41.90, 	12.45]]
 
 
@@ -147,7 +122,18 @@ VEN = [10.49, -66.88]
 
 capitalesSud = [ARG, BOL, BRA, CHI, COL, ECU, GUA, PAR, PER, SUR, URU, VEN]
 
+print "Distancia entre las ciudades de Europa"
+
 for i,item1 in enumerate(capitalesEur):
     for j,item2 in enumerate(capitalesEur):
         if(i != j):
             print i+1,j+1, round(great_circle(item1, item2).km)
+    print ""
+
+print "Distancia entre las ciudades de Sudamerica"
+
+for i,item1 in enumerate(capitalesSud):
+    for j,item2 in enumerate(capitalesSud):
+        if(i != j):
+            print i+1,j+1, round(great_circle(item1, item2).km)
+    print ""
